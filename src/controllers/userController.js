c0bbcb50826ea0f3f365a41da6384fc5ca460557
Altovisk <ericaltao13@gmail.com>
2023-10-20 09:44:23 -0300

    criacao de middleware de validacao de rota para cadastro

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,21 +1,9 @@
 const User = require('../models/users');
+const bcrypt = require('bcrypt');
 
 const userController = {
-  teste: (req, res) => {
-    const novoUsuario = new User({
-      nome: 'Nome do Usuário',
-      email: 'usuario@email.com',
-      senha: 'senha123',
-      projetos: [],
-    });
-
-    novoUsuario.save()
-      .then((user) => {
-        console.log('Usuário cadastrado com sucesso:', user);
-      })
-      .catch((error) => {
-        console.error('Erro ao cadastrar o usuário:', error);
-      });
+  signup: (req, res) => {
+    
   },
 };
 
---
+++ src/middlewares/singupValidation.js
@@ -0,0 +1,30 @@
+const { body } = require('express-validator');
+const User = require('../models/users');
+
+const validation = [
+    body('username')
+        .notEmpty().withMessage('Insira um nick para o usuário').bail()
+        .isLength({ max: 15 }).withMessage('Máximo de caracteres permitidos (10)')
+        .custom(async (value) => {
+            const user = await User.findOne({ username: value });
+            if (user) {
+                throw new Error('O usuário já está em uso');
+            }
+            return true;
+        }),
+    body('email')
+        .notEmpty().withMessage('Insira um email').bail()
+        .isEmail().withMessage('Insira um formato de email válido')
+        .custom(async (value) => {
+            const email = await User.findOne({ email: value });
+            if (email) {
+                throw new Error('Este email já está em uso');
+            }
+            return true;
+        }),
+    body('password')
+        .notEmpty().withMessage('Insira uma senha').bail()
+        .isLength({ min: 6 }).withMessage('Sua senha deve ter no mínimo 6 caracteres'),
+];
+
+module.exports = validation;
